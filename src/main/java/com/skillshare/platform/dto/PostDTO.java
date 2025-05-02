62d4e670a0ef84a873e247c043353a533eedd489
dinusha_d <dinushaariyarathna2@gmail.com>
2025-05-02 23:21:51 +0530

    Post DTO class created.

---
+++ src/main/java/com/skillshare/platform/dto/PostDTO.java
@@ -0,0 +1,18 @@
+package com.skillshare.platform.dto;
+
+import lombok.Data;
+
+/**
+ * @AUTHOR : Dinusha Ariyarathna
+ * @DATE : 5/2/2025
+ * @PROJECT : platform
+ */
+
+@Data
+public class PostDTO {
+    private String title;
+    private String description;
+    private String mediaUrl1;
+    private String mediaUrl2;
+    private String mediaUrl3;
+}
