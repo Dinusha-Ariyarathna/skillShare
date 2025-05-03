1b8cfbb1806f500a3225d7ae4dd377cea46ddee0
dinusha_d <dinushaariyarathna2@gmail.com>
2025-05-03 22:47:40 +0530

    Made changes in comment entity and CommentRepository

--- src/main/java/com/skillshare/platform/repository/CommentRepository.java
+++ src/main/java/com/skillshare/platform/repository/CommentRepository.java
@@ -2,9 +2,11 @@ package com.skillshare.platform.repository;
 
 import com.skillshare.platform.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findByPostIdOrderByCreatedAtAsc(Long postId);
 }
